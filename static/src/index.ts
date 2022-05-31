c0239ebd3f536d02c39f76a481a74193c88e6475
Tigran <abvgdesh3@gmail.com>
2022-06-01 01:37:42 +0300

    not worked

--- static/src/index.ts
+++ static/src/index.ts
@@ -1,15 +1,26 @@
-import { listen } from '@codingame/monaco-jsonrpc';
-import * as monaco from 'monaco-editor'
-import {
-    MonacoLanguageClient, MessageConnection, CloseAction, ErrorAction,
-    MonacoServices, createConnection
-} from 'monaco-languageclient';
+import ReconnectingWebsocket = require("reconnecting-websocket");
 // @ts-ignore
 import normalizeUrl = require('normalize-url');
+
+require('monaco-editor');
+(self as any).MonacoEnvironment = {
+    getWorkerUrl: () => '/editor/scripts/editor.worker.bundle.js'
+}
+import {listen} from '@codingame/monaco-jsonrpc';
+import * as monaco from 'monaco-editor-core'
+
+import {
+    CloseAction,
+    createConnection,
+    ErrorAction,
+    MessageConnection,
+    MonacoLanguageClient,
+    MonacoServices
+} from 'monaco-languageclient';
+
 const ReconnectingWebSocket = require('reconnecting-websocket');
 
 const editor = monaco.editor.create(document.getElementById("container")!, {
-    model: monaco.editor.createModel("", 'cpp', monaco.Uri.parse('file://D://Projects/CodeExample/example.cpp')),
     theme: "vs-dark",
     accessibilityPageSize: 0,
     minimap: {
@@ -18,26 +29,16 @@ const editor = monaco.editor.create(document.getElementById("container")!, {
     mouseWheelZoom: true,
     lightbulb: {
         enabled: true,
-    }
+    },
+    inlayHints: {
+        enabled: true,
+    },
 });
+
 
 window.onresize = function () {
     editor.layout()
 }
-// @ts-ignore
-MonacoServices.install(monaco, {rootUri: "file://D://Projects/CodeExample"})
-
-const url = createUrl('ws://localhost:3000/clangd')
-const webSocket = createWebSocket(url);
-
-listen({
-    webSocket,
-    onConnection: connection => {
-        const languageClient = createLanguageClient(connection, 'cpp');
-        const disposable = languageClient.start();
-        connection.onClose(() => disposable.dispose());
-    }
-});
 
 function createLanguageClient(connection: MessageConnection, language: string): MonacoLanguageClient {
     return new MonacoLanguageClient({
@@ -78,3 +79,73 @@ function createWebSocket(url: string): WebSocket {
     };
     return new ReconnectingWebSocket(url, [], socketOptions);
 }
+
+let webSocket: ReconnectingWebsocket | null = null;
+// @ts-ignore
+/*let monacoService: MonacoServices | null = null;*/
+
+function change_monaco_lang(lang: string) {
+    const url = createUrl(`ws://localhost:3000/${lang}`)
+    if (webSocket != null) {
+        const socket: any = webSocket;
+        socket.close(1000, '', {keepClosed: true});
+        webSocket = null;
+    }
+    /*if (monacoService != null){
+        monacoService.dispose()
+    }*/
+
+    webSocket = createWebSocket(url);
+
+    listen({
+        webSocket,
+        onConnection: connection => {
+            const languageClient = createLanguageClient(connection, lang);
+            const disposable = languageClient.start();
+            connection.onClose(() => {
+                disposable.dispose()
+                console.log('closing websocket')
+            });
+        }
+    });
+    let uri = "";
+    let rootPath = "";
+    switch (lang) {
+        case "java":
+            uri = "file:./file.java"
+            rootPath = "file:///D:/Projects/WebEditorServices/jsonrpc-ws-proxy/JAVA_PROJECT"
+            break;
+        case "cpp":
+            uri = "file://./file.cpp"
+            rootPath = "file:///D:/Projects/WebEditorServices/jsonrpc-ws-proxy/CPP_PROJECT"
+            break;
+        case "csharp":
+            uri = "file://./file.cs"
+            rootPath = "file:///D:/Projects/WebEditorServices/jsonrpc-ws-proxy/CSHARP_PROJECT"
+            break;
+    }
+    ReplaceModel(lang, uri, rootPath)
+}
+
+function ReplaceModel(lang: string, uri: string, rootPath: string) {
+    const oldModel = editor.getModel()
+    let oldVal = "";
+    if (oldModel) {
+        oldVal = oldModel.getValue()
+        oldModel.dispose()
+    }
+    editor.setModel(monaco.editor.createModel(oldVal, lang, monaco.Uri.parse(uri)))
+    let props = {rootUri: rootPath}
+    console.log(editor)
+    console.log(props)
+    //@ts-ignore
+    MonacoServices.install(editor, props)
+}
+
+document.getElementById('langSelector')!.addEventListener('change', function () {
+    // @ts-ignore
+    let lang = this.value
+    change_monaco_lang(lang as string)
+})
+
+change_monaco_lang('cpp')
--- static/src/main.ts
+++ static/src/main.ts
@@ -1,5 +1,1 @@
-require('monaco-editor');
-(self as any).MonacoEnvironment = {
-    getWorkerUrl: () => '/editor/scripts/editor.worker.bundle.js'
-}
 require('./index');
